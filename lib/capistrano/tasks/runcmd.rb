0104b58fc05786596ba04193d4f8628a431ba492
Sal <0xack13@gmail.com>
2018-06-09 03:30:52 +1000

    Initial commit

---
+++ lib/capistrano/tasks/runcmd.rb
@@ -0,0 +1,16 @@
+server 'localhost', roles: [:db, :workers]
+desc "Report Uptimes"
+task :uptime do
+  on roles(:all) do |host|
+    execute :any_command, "with args", :here, "and here"
+    info "Host #{host} (#{host.roles.to_a.join(', ')}):\t#{capture(:uptime)}"
+  end
+end
+
+task :foo do
+    puts "foo"
+end
+
+task :foo do
+    puts "bar"
+end
